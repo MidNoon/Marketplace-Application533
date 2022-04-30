d56a97345d3a1c8b215d88cc176eec8ab8f4d725
ShermanRich <80287905+ShermanRich@users.noreply.github.com>
2022-04-30 16:24:04 -0500

    Update

--- MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/ItemManagementActivity.java
+++ MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/ItemManagementActivity.java
@@ -4,18 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ItemManagementActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Button button_add_new_product;
+    private Button button_view_sold_items;
+    private Button button_view_active_items;
+    private Button button_view_bought_items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_managment);
         mAuth = FirebaseAuth.getInstance();
+        button_add_new_product = findViewById(R.id.add_new_product);
+        button_view_sold_items = findViewById(R.id.view_sold_product);
+        button_view_active_items = findViewById(R.id.view_active_product);
+        button_view_bought_items = findViewById(R.id.view_bought_products);
+
+        button_add_new_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemManagementActivity.this, UAddItemActivity.class));
+            }
+        });
+        button_view_sold_items.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemManagementActivity.this, USoldItemsActivity.class));
+            }
+        });
+        button_view_active_items.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemManagementActivity.this, UViewItemsActivity.class));
+            }
+        });
+        button_view_bought_items.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemManagementActivity.this, UBoughtItemsActivity.class));
+            }
+        });
     }
     @Override
     public void onStart(){
