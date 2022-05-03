322e30b0338cec962cbc6a59b477f15cd23772e9
ShermanRich <80287905+ShermanRich@users.noreply.github.com>
2022-05-03 16:43:35 -0500

    Update

--- MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/UAddItemActivity.java
+++ MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/UAddItemActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class UAddItemActivity extends AppCompatActivity {
+    private String ProductName, ProductDescription, Price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_add_item);
+
+        ProductName = FirebaseDatabase.getInstance().getReference().child("Product Name");
+        ProductDescription  = FirebaseDatabase.getInstance().getReference().child("Product Description");
+        Price = FirebaseDatabase.getInstance().getReference().child("Price");
+
+
+    }
+    private void SaveProductInfoToDatabase()
+    {
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("product", ProductName);
+        productMap.put("description", ProductDescription);
+        productMap.put("price", Price);
+
     }
 }
