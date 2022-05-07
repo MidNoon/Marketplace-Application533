21266f3b9462e9738f49b692a2f36c0530753cbe
MidNoon <75605062+MidNoon@users.noreply.github.com>
2022-05-06 22:38:59 -0500

    Browse Activity Update
    Browse Activity Update
    No fragment in this part

--- MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/BrowseActivity.java
+++ MarketplaceAplo5/app/src/main/java/com/example/marketplaceaplo5/BrowseActivity.java
@@ -1,21 +1,44 @@
 package com.example.marketplaceaplo5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.marketplaceaplo5.Classes.Products;
+import com.example.marketplaceaplo5.ViewHolderClasses.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.squareup.picasso.Picasso;
 
 public class BrowseActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
         mAuth = FirebaseAuth.getInstance();
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
     @Override
     public void onStart(){
@@ -24,5 +47,37 @@ public class BrowseActivity extends AppCompatActivity {
         if(currentUser == null) {
             startActivity(new Intent(BrowseActivity.this, LoginActivity.class));
         }
+        FirebaseRecyclerOptions<Products> query =
+                new FirebaseRecyclerOptions.Builder<Products>().setQuery(ProductsRef, Products.class).build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(query) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
+                holder.vproductName.setText(model.getProductName());
+                holder.vproductPrice.setText("Price: "+model.getPrice());
+                holder.vproductDescription.setText(model.getDescription());
+                Picasso.get().load(model.getImage()).into(holder.vproductImage);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(BrowseActivity.this, DetailBrowseFragment.class);
+                        intent.putExtra("pid",model.getPid());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.product_item_layout,viewGroup,false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 }
